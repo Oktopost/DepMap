5ab2b5264de5c6a3d7ab6d94d298cb7037d22fbb
Alexey Puchkov <alexey@oktopost.com>
2016-07-19 15:16:34 +0300

    Add RecursiveFolderIterator object

---
+++ src/DepMap/Iterator/RecursiveFolderIterator.php
@@ -0,0 +1,134 @@
+<?php
+namespace DepMap\Iterator;
+
+
+use DepMap\Filter\FilterMap;
+use DepMap\Filter\FilterFileReader;
+
+
+class RecursiveFolderIterator
+{
+	private $rootDir;
+	private $filterMapFormat = '';
+	
+	/** @var FilterMap[] */
+	private $filters = [];
+	
+	
+	/**
+	 * @param string $dir
+	 * @return int Number of loaded FilterMaps
+	 */
+	private function loadMapFileInDirectory($dir)
+	{		
+		if (!$this->filterMapFormat) return 0;
+		
+		$loaded = 0;
+		
+		foreach (glob("$dir/{$this->filterMapFormat}") as $file)
+		{
+			$mapLoader = new FilterFileReader($file);
+			$map = $mapLoader->read();
+			
+			if ($map)
+			{
+				$this->filters[] = $map;
+				$loaded++;
+			}
+		}
+		
+		return $loaded;
+	}
+	
+	/**
+	 * @param string $file
+	 * @return bool
+	 */
+	private function isIncluded($file)
+	{
+		foreach ($this->filters as $filter)
+		{
+			if (!$filter->isIncluded($file)) 
+			{
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * @param string $dir
+	 * @return \Generator
+	 */
+	private function filterDirectory($dir)
+	{
+		foreach (glob("$dir/*") as $item)
+		{
+			if (!$this->isIncluded($item)) continue;
+			
+			if (is_file($item))
+			{
+				if (fnmatch($this->filterMapFormat, basename($item)))
+				{
+					continue;
+				}
+				
+				yield $item;
+			}
+			else
+			{
+				foreach ($this->iterateDirectory($item) as $subItem)
+				{
+					yield $subItem;
+				}
+			}
+		}
+	}
+	
+	/**
+	 * @param string $dir
+	 * @return \Generator
+	 */
+	private function iterateDirectory($dir)
+	{
+		$loadedFilters = $this->loadMapFileInDirectory($dir);
+		
+		foreach ($this->filterDirectory($dir) as $item)
+		{
+			yield $item;
+		}
+		
+		if ($loadedFilters)
+		{
+			$this->filters = array_slice($this->filters, 0, count($this->filters) - $loadedFilters);
+		}
+	}
+	
+	
+	/**
+	 * @param string $rootDir
+	 */
+	public function __construct($rootDir)
+	{
+		$this->rootDir = $rootDir;
+	}
+	
+	/**
+	 * @param string $format
+	 * @return static
+	 */
+	public function setFilterMapFormat($format)
+	{
+		$this->filterMapFormat = $format;
+		return $this;
+	}
+	
+	/**
+	 * @return \Generator
+	 */
+	public function find()
+	{
+		return $this->iterateDirectory($this->rootDir);
+	}
+}
---
+++ test/DepMap/Iterator/RecursiveFolderIteratorTest.php
@@ -0,0 +1,121 @@
+<?php
+namespace DepMap\Iterator;
+
+
+class RecursiveFolderIteratorTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * @param string $item
+	 * @return string
+	 */
+	private function getPathToTestItem($item)
+	{
+		return realpath(__DIR__ . '/RecursiveFolderIteratorTest') . "/$item";
+	}
+	
+	/**
+	 * @param string $dir
+	 * @return string
+	 */
+	private function getTestDirectory($dir)
+	{
+		$dir = $this->getPathToTestItem($dir);
+		
+		if (!is_dir($dir))
+		{
+			mkdir($dir);
+		}
+		
+		return $dir;
+	}
+	
+	/**
+	 * @param array $items
+	 * @param string $dir
+	 * @param string $filterFormat
+	 */
+	private function assertContainItems(array $items, $dir, $filterFormat = 'deploy.map')
+	{
+		$foundItems = $this->runForFolder($dir, $filterFormat);
+		
+		sort($items);
+		sort($foundItems);
+		array_walk($items, 
+			function(&$item)
+				use ($dir)
+			{
+				$item = $this->getPathToTestItem($dir . '/' . $item);
+			});
+		
+		$this->assertEquals($items, $foundItems);
+	}
+	
+	/**
+	 * @param string $dir
+	 * @param string $filterFormat
+	 * @return array
+	 */
+	private function runForFolder($dir, $filterFormat = 'deploy.map')
+	{
+		$dir = $this->getTestDirectory($dir);
+		$result = [];
+		
+		$it = new RecursiveFolderIterator($dir);
+		$it->setFilterMapFormat($filterFormat);
+		
+		foreach ($it->find() as $item)
+		{
+			$result[] = $item;
+		}
+		
+		return $result;
+	}
+	
+	
+	public function test_setFilterMapFormat_ReturnSelf()
+	{
+		$it = new RecursiveFolderIterator('a');
+		$this->assertSame($it, $it->setFilterMapFormat('a'));
+	}
+	
+	
+	public function test_find_EmptyDirectory_NothingFound()
+	{
+		$this->assertEquals([], $this->runForFolder('Empty'));
+	}
+	
+	public function test_find_DirectoryWithoutFilters_ItemsLoaded()
+	{
+		$this->assertContainItems(['a', 'b'], 'NoFilters');
+	}
+	
+	public function test_find_FilesInSubDirectoriesLoaded()
+	{
+		$this->assertContainItems(['a/a'], 'SubFoldersLoaded');
+	}
+	
+	public function test_find_FilterFileLoaded()
+	{
+		$this->assertContainItems(['a'], 'SimpleFilterFile');
+	}
+	
+	public function test_find_NumberOfFilterFiles_AllLoaded()
+	{
+		$this->assertContainItems(['a', 'd'], 'NumberOfFilterFiles', 'deploy.map*');
+	}
+	
+	public function test_find_FilterInSubDirectory_FilterLoaded()
+	{
+		$this->assertContainItems(['a/a'], 'SubDirectoryFilter');
+	}
+	
+	public function test_find_FilterInAnotherSubDirectory_FilterDoesNotAffectOtherDirectories()
+	{
+		$this->assertContainItems(['a/a', 'b/b'], 'FilterInAnother');
+	}
+	
+	public function test_find_ParentFilterMapsHasHigherPriority()
+	{
+		$this->assertContainItems(['a/a'], 'HigherMap');
+	}
+}
